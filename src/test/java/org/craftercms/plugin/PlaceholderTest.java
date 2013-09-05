821aafd2dcfec93dd3ee3b8696a3edc94edcfa10
Carlos Ortiz <carlos.ortiz@craftercms.org>
2013-09-05 13:58:03 -0600

    Added simple test to avoid build to break

---
+++ src/test/java/org/craftercms/plugin/PlaceholderTest.java
@@ -0,0 +1,21 @@
+package org.craftercms.plugin;
+
+import org.junit.Test;
+
+/**
+ * <b>
+ * This is to avoid the build
+ * to break due non exist test
+ * TO BE DELETE as soon actual
+ * test exist </b>
+ */
+public class PlaceholderTest {
+
+    @Test
+    public void testOk() throws Exception {
+        // This is while non actual test exist
+        // avoid whold build to fail on CI server
+
+
+    }
+}
